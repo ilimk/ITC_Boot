5c5e1f8646a3f586da94a90b80218f8f92e5636b
ilimk <43859355+ilimk@users.noreply.github.com>
2022-02-11 21:17:00 +0600

    Delete core.py

--- core.py
+++
@@ -1,7 +0,0 @@
-a = ['53:14', '245435:543645']
-for i in a:
-    ind = i.index(':')
-    score1 = i[0:ind]
-    score2 = i[ind+1:len(i)]
-    print('score1 :', score1)
-    print('score2 :', score2)
